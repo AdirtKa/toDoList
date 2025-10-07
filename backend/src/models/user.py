2083322a5c85446f8248afb3a452e6e956436ff3
AdirtKa <AdirtKa@yandex.ru>
2025-10-07 21:46:31 +1000

    change max password length

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -15,4 +15,4 @@ class User(Base):
     __tablename__ = 'user'
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(50), nullable=False)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
