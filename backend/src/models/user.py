36c490876bd416d30d005b184818e47f4050c2bc
adirtka <AdirtKa@yandex.ru>
2025-10-07 12:36:26 +1000

    add user model in database

---
+++ backend/src/models/user.py
@@ -0,0 +1,18 @@
+"""file for a user model."""
+
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models.base import Base
+
+
+class User(Base):
+    """Model for a user."""
+
+    __tablename__ = 'user'
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
